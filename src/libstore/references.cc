f546f4b8356ccceb21e2aff355daae1b7a7b3ee7
Eelco Dolstra <edolstra@gmail.com>
2026-05-28 15:30:41 +0200

    HashModuloSink: Hash the positions of self-references again
    This was accidentally lost in
    https://github.com/NixOS/nix/commit/3ebe1341abe1b0ad59bd4925517af18d9200f818
    (Nix 2.17.0), allowing output paths with self-references to hash to
    the same store path as output paths that are identical except that
    some of the self-references are zeroed out.
    
    Note: this will cause self-referential store paths to differ from
    those produced by Nix 2.17-2.34.
    
    Fixes #15837.

--- src/libstore/references.cc
+++ src/libstore/references.cc
@@ -72,7 +72,7 @@ void RewritingSink::operator()(std::string_view data)
     std::string s(prev);
     s.append(data);
 
-    s = rewriteStrings(s, rewrites);
+    s = rewriteStrings(s, rewrites, &matches, pos);
 
     prev = s.size() < maxRewriteSize ? s
            : maxRewriteSize == 0     ? ""
@@ -97,6 +97,7 @@ void RewritingSink::flush()
 
 HashModuloSink::HashModuloSink(HashAlgorithm ha, const std::string & modulus)
     : hashSink(ha)
+    // Zero out self-references (the "modulus").
     , rewritingSink(modulus, std::string(modulus.size(), 0), hashSink)
 {
 }
